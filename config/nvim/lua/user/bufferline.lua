8294e6a4de87c0e9289e0854f878b6c3cc021563
James Grundner <james@jgrundner.com>
2023-11-19 11:20:43 -0500

    updating bufferline config

--- config/nvim/lua/user/bufferline.lua
+++ config/nvim/lua/user/bufferline.lua
@@ -12,18 +12,13 @@ bufferline.setup {
     right_mouse_command = "Bdelete! %d", -- can be a string | function, see "Mouse actions"
     left_mouse_command = "buffer %d", -- can be a string | function, see "Mouse actions"
     middle_mouse_command = nil, -- can be a string | function, see "Mouse actions"
-    -- NOTE: this plugin is designed with this icon in mind,
-    -- and so changing this is NOT recommended, this is intended
-    -- as an escape hatch for people who cannot bear it for whatever reason
     indicator = {
       style = 'icon',
       icon ="▎"
     },
-    --[[ buffer_close_icon = "", ]]
     buffer_close_icon = '',
     modified_icon = "●",
     close_icon = "",
-    -- close_icon = '',
     left_trunc_marker = "",
     right_trunc_marker = "",
     max_name_length = 30,
@@ -31,14 +26,22 @@ bufferline.setup {
     tab_size = 21,
     diagnostics = "nvim_lsp", -- false | "nvim_lsp" | "coc",
     diagnostics_update_in_insert = false,
-    diagnostics_indicator = function(count, level, diagnostics_dict, context)
+    diagnostics_indicator = function(count, level, _, context)
       local icon = level:match("error") and " " or " "
       if context.buffer:current() then
         return ""
       end
       return " " .. icon .. count
     end,
-    offsets = { { filetype = "File Explorer", text = "", padding = 1 } },
+    offsets = {
+      {
+        filetype = "NvimTree",
+        text = "File Explorer",
+        highlight = "Directory",
+        separator = true,
+        padding = 1
+      }
+    },
     show_buffer_icons = true,
     show_buffer_close_icons = true,
     show_close_icon = true,
@@ -50,140 +53,4 @@ bufferline.setup {
     enforce_regular_tabs = false,
     always_show_bufferline = true,
   },
-  --[[ highlights = { ]]
-  --[[   fill = { ]]
-  --[[     fg = { attribute = "fg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[   }, ]]
-  --[[   background = { ]]
-  --[[     fg = { attribute = "fg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[   }, ]]
-  --[[]]
-  --[[   buffer_selected = { ]]
-  --[[     fg = {attribute='fg',highlight='LspDiagnosticsDefaultHint'}, ]]
-  --[[     -- bg = {attribute='bg',highlight='#0000ff'}, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLineSel" }, ]]
-  --[[     bold = true ]]
-  --[[   }, ]]
-  --[[   buffer_visible = { ]]
-  --[[     fg = { attribute = "fg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[   }, ]]
-  --[[]]
-  --[[   diagnostic_selected = { ]]
-  --[[     fg = {attribute='fg',highlight='LspDiagnosticsDefaultHint'}, ]]
-  --[[     bg = {attribute='bg',highlight='#00ff00'}, ]]
-  --[[     bold = true ]]
-  --[[   }, ]]
-  --[[   diagnostic_visible = { ]]
-  --[[     fg = {attribute='fg',highlight='LspDiagnosticsDefaultHint'}, ]]
-  --[[     bg = {attribute='bg',highlight='#00ff00'}, ]]
-  --[[   }, ]]
-  --[[]]
-  --[[   info_selected = { ]]
-  --[[     fg = {attribute='fg',highlight='LspDiagnosticsDefaultInfo'}, ]]
-  --[[     bg = {attribute='bg',highlight='#73D7FF'}, ]]
-  --[[     bold = true ]]
-  --[[   }, ]]
-  --[[   info_visible = { ]]
-  --[[     fg = {attribute='fg',highlight='LspDiagnosticsDefaultInfo'}, ]]
-  --[[     bg = {attribute='bg',highlight='#73D7FF'}, ]]
-  --[[   }, ]]
-  --[[]]
-  --[[   warning_selected = { ]]
-  --[[     fg = {attribute='fg',highlight='LspDiagnosticsDefaultWarn'}, ]]
-  --[[     bg = {attribute='bg',highlight='#ffff00'}, ]]
-  --[[     bold = true ]]
-  --[[   }, ]]
-  --[[   warning_visible = { ]]
-  --[[     fg = {attribute='fg',highlight='LspDiagnosticsDefaultWarn'}, ]]
-  --[[     bg = {attribute='bg',highlight='#ffff00'}, ]]
-  --[[   }, ]]
-  --[[]]
-  --[[   error_selected = { ]]
-  --[[     fg = {attribute='fg',highlight='LspDiagnosticsDefaultError'}, ]]
-  --[[     bg = {attribute='bg',highlight='#ff0000'}, ]]
-  --[[     bold = true ]]
-  --[[   }, ]]
-  --[[   error_visible = { ]]
-  --[[     fg = {attribute='fg',highlight='LspDiagnosticsDefaultError'}, ]]
-  --[[     bg = {attribute='bg',highlight='#ff0000'}, ]]
-  --[[   }, ]]
-  --[[]]
-  --[[   close_button = { ]]
-  --[[     fg = { attribute = "fg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[   }, ]]
-  --[[   close_button_visible = { ]]
-  --[[     fg = { attribute = "fg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[   }, ]]
-  --[[   -- close_button_selected = { ]]
-  --[[   --   fg = {attribute='fg',highlight='TabLineSel'}, ]]
-  --[[   --   bg ={attribute='bg',highlight='TabLineSel'} ]]
-  --[[   --   }, ]]
-  --[[]]
-  --[[   tab_selected = { ]]
-  --[[     fg = { attribute = "fg", highlight = "Normal" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "Normal" }, ]]
-  --[[   }, ]]
-  --[[   tab = { ]]
-  --[[     fg = { attribute = "fg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[   }, ]]
-  --[[   tab_close = { ]]
-  --[[     fg = { attribute = "fg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[   }, ]]
-  --[[]]
-  --[[   duplicate_selected = { ]]
-  --[[     fg = { attribute = "fg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[     italic = true ]]
-  --[[   }, ]]
-  --[[   duplicate_visible = { ]]
-  --[[     fg = { attribute = "fg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[     italic = true ]]
-  --[[   }, ]]
-  --[[   duplicate = { ]]
-  --[[     fg = { attribute = "fg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[     italic = true ]]
-  --[[   }, ]]
-  --[[]]
-  --[[   modified = { ]]
-  --[[     fg = { attribute = "fg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[   }, ]]
-  --[[   modified_selected = { ]]
-  --[[     fg = { attribute = "fg", highlight = "Normal" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "Normal" }, ]]
-  --[[   }, ]]
-  --[[   modified_visible = { ]]
-  --[[     fg = { attribute = "fg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[   }, ]]
-  --[[]]
-  --[[   separator = { ]]
-  --[[     fg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[   }, ]]
-  --[[   separator_selected = { ]]
-  --[[     fg = { attribute = "bg", highlight = "Normal" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "Normal" }, ]]
-  --[[   }, ]]
-  --[[   separator_visible = { ]]
-  --[[     fg = {attribute='bg',highlight='TabLine'}, ]]
-  --[[     bg = {attribute='bg',highlight='TabLine'} ]]
-  --[[     }, ]]
-  --[[   indicator_selected = { ]]
-  --[[     -- fg = { attribute = "fg", highlight = "LspDiagnosticsDefaultHint" }, ]]
-  --[[     -- fg = { attribute = "fg", highlight = "Normal" }, ]]
-  --[[     -- bg = { attribute = "bg", highlight = "Normal" }, ]]
-  --[[     fg = { attribute = "fg", highlight = "TabLine" }, ]]
-  --[[     bg = { attribute = "bg", highlight = "TabLine" }, ]]
-  --[[   }, ]]
-  --[[ }, ]]
 }
